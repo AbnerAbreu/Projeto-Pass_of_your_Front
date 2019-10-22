3eeefe844244d2f9aa5575b2bca17a4a34a529fd
AbnerAbreu <abnerdrift94@gmail.com>
2019-10-22 18:03:16 -0300

    alteration scss

--- src/Componentes/Home.js
+++ src/Componentes/Home.js
@@ -13,24 +13,21 @@ const Home = ({texto}) => {
     return (
         <div className="Home">
            <header>
-               <div className="titulo">
-               <h1>O  que não é util para você, <br/>
-                 pode ser util para escolas carentes.</h1>
-
-               </div>
+               <h1>O  QUE NÃO É UTIL PARA VOCÊ, <br/>
+                   PODE SER UTIL PARA ESCOLAS CARENTES</h1>
            </header>
-           <section>
-               <div className="equipe">
-           <h1>Equipe:</h1>
-               </div>
             <div>
-                <Imagem conteudo={Abner} texto={"Abner Abreu"}/>
-                <Imagem conteudo={samanta} texto={"Samanta Souza"}/>
-                <Imagem conteudo={thiago2} texto={"Thiago Mendes"}/>
-                <Imagem conteudo={roger} texto={"Roger"}/>
-                <Imagem conteudo={larissa} texto={"Larissa Leonso"}/>
-                <Imagem conteudo={beatriz} texto={"Beatriz Pavan "}/>
+                <h1>Equipe:</h1>
             </div>
+           <section>
+                <div>
+                    <Imagem conteudo={Abner} texto={"Abner Abreu"}/>
+                    <Imagem conteudo={samanta} texto={"Samanta Souza"}/>
+                    <Imagem conteudo={thiago2} texto={"Thiago Mendes"}/>
+                    <Imagem conteudo={roger} texto={"Roger"}/>
+                    <Imagem conteudo={larissa} texto={"Larissa"}/>
+                    <Imagem conteudo={beatriz} texto={"Batriz pavan "}/>
+                </div>
 
            </section>
 
