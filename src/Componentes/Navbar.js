7a62cb41dbd84dee94e02af101054c7d0c973196
samantasouza357 <samantasouza931@gmail.com>
2019-10-23 17:06:49 -0300

    adicionar florzinha

--- src/Componentes/Navbar.js
+++ src/Componentes/Navbar.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import Imagem from '../Componentes/Imagem';
+import carnation from './imagem/carnation.png';
 import './Navbar.scss';
 
 const Navbar = () => {
     return (
         <div className="Navbar">
             <nav>
+                <div className="flor">
+                <Imagem conteudo={carnation} />
+                </div>
             <Link  to="/">Home</Link>
             <Link  to="/cadastrar">Cadastrar</Link>
             <Link  to="/login">Escolas</Link>
